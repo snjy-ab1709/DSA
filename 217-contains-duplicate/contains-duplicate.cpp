5f8a3b9cf478fecf42fd9e09fbdfb583193e5ca1
snjy-ab1709 <sanjaybes2802@gmail.com>
2026-09-11 16:56:16 +0530

    Time: 76 ms (37.02%) | Memory: 111.4 MB (28.31%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+     unordered_map<int, int> mpp; 
+      for (int num : nums) {
+            mpp[num]++;
+            if (mpp[num] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
