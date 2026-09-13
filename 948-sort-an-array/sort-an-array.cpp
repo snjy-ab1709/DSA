0d039baa7dce0809d5458c27a8f1087a46ad755e
snjy-ab1709 <sanjaybes2802@gmail.com>
2026-09-13 23:02:44 +0530

    Time: 586 ms (44.95%) | Memory: 295.6 MB (25.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,47 +1,47 @@
 class Solution {
 private:
-    void merge(vector<int>& arr, int low, int mid, int high) {
+    void merge(vector<int>& nums, int low, int mid, int high) {
         vector<int> temp;
         int left = low;
         int right = mid + 1;
 
         while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.push_back(arr[left]);
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left]);
                 left++;
             } else {
-                temp.push_back(arr[right]);
+                temp.push_back(nums[right]);
                 right++;
             }
         }
 
         while (left <= mid) {
-            temp.push_back(arr[left]);
+            temp.push_back(nums[left]);
             left++;
         }
 
         while (right <= high) {
-            temp.push_back(arr[right]);
+            temp.push_back(nums[right]);
             right++;
         }
 
         for (int i = low; i <= high; i++) {
-            arr[i] = temp[i - low];
+            nums[i] = temp[i - low];
         }
     }
 
-    void mergeSort(vector<int>& arr, int low, int high) {
+    void mergeSort(vector<int>& nums, int low, int high) {
         if (low >= high)
             return;
 
-        int mid = low + (high - low) / 2; 
+        int mid = (low + high)/2; 
 
         
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid + 1, high);
 
      
-        merge(arr, low, mid, high);
+        merge(nums, low, mid, high);
     }
 
 public:
